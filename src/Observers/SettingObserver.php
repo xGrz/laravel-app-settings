936730692973bfec858f1f4635dfeaf51afeb18d
xGrz <33755434+xGrz@users.noreply.github.com>
2024-02-12 19:43:53 +0100

    Removed observer bodies.

--- src/Observers/SettingObserver.php
+++ src/Observers/SettingObserver.php
@@ -2,22 +2,21 @@
 
 namespace xGrz\LaravelAppSettings\Observers;
 
-use xGrz\LaravelAppSettings\Support\Services\SettingsCacheService;
 
 class SettingObserver
 {
     public function created(): void
     {
-        SettingsCacheService::resetCache();
+
     }
 
     public function updated(): void
     {
-        SettingsCacheService::resetCache();
+
     }
 
     public function deleted(): void
     {
-        SettingsCacheService::resetCache();
+
     }
 }
