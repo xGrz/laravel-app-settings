a68ed07b6787ba9c774cfb6098404e3589a5747c
xGrz <33755434+xGrz@users.noreply.github.com>
2024-02-12 19:38:06 +0100

    Added dynamic table name in migration.
    Table name is taken from Setting model.

--- database/migrations/2024_02_12_124751_create_settings_table.php
+++ database/migrations/2024_02_12_124751_create_settings_table.php
@@ -3,20 +3,22 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use xGrz\LaravelAppSettings\Enums\SettingValueType;
+use xGrz\LaravelAppSettings\Models\Setting;
 
 return new class extends Migration {
 
 
     public function up(): void
     {
-        Schema::create('settings', function (Blueprint $table) {
+        Schema::create((new Setting())->getTable(), function (Blueprint $table) {
             $table->id();
             $table->string('groupName');
             $table->string('keyName');
             $table->string('key')->virtualAs("CONCAT(`groupName`, '.', `keyName`)")->unique();
             $table->text('value')->nullable();
             $table->text('description')->nullable();
-            // $table->integer('type')->default(SettingValueType::Text);
+            $table->integer('type')->default(SettingValueType::Text);
             $table->timestamps();
         });
 
@@ -25,6 +27,6 @@ return new class extends Migration {
 
     public function down(): void
     {
-        Schema::dropIfExists('settings');
+        Schema::dropIfExists((new Setting())->getTable());
     }
 };
