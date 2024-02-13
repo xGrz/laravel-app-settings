2168261e90e31a2c9f92ae5f701b2ee36f218a40
xGrz <33755434+xGrz@users.noreply.github.com>
2024-02-13 11:48:41 +0100

    Added store Settings service with validation

---
+++ src/Support/Services/StoreSettingService.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace xGrz\LaravelAppSettings\Support\Services;
+
+use xGrz\LaravelAppSettings\Exceptions\SettingsKeyNotFoundException;
+use xGrz\LaravelAppSettings\Exceptions\SettingValueValidationException;
+use xGrz\LaravelAppSettings\Models\Setting;
+use xGrz\LaravelAppSettings\Http\Requests\UpdateSettingRequest;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+
+class StoreSettingService
+{
+    protected ?Setting $setting;
+
+    public function __construct(int|string $keyIdent)
+    {
+        is_int($keyIdent)
+            ? self::loadById($keyIdent)
+            : self::loadByKey($keyIdent);
+    }
+
+    public static function find(int|string $keyIdent): StoreSettingService
+    {
+        return new self($keyIdent);
+    }
+
+    private function loadByKey(string $key): ?Setting
+    {
+        $this->setting = Setting::where('key', $key)->first();
+        if (!$this->setting) {
+            SettingsKeyNotFoundException::missingKey($key);
+        }
+        return $this->setting;
+    }
+
+    private function loadById(int $id)
+    {
+        $this->setting = Setting::find($id);
+        if (!$this->setting) {
+            SettingsKeyNotFoundException::missingKey($id);
+        }
+        return $this->setting;
+    }
+
+    public function update(array $settingData)
+    {
+        $updateData = [];
+        if (isset($settingData['value'])) {
+            try {
+                $valueValidator = Validator::make(
+                    ['value' => $settingData['value']],
+                    ['value' => UpdateSettingRequest::getValueValidationRules($this->setting)]
+                );
+                $updateData = array_merge($updateData, $valueValidator->validate());
+            } catch (ValidationException $e) {
+                throw new SettingValueValidationException($e->getMessage());
+            }
+
+        }
+
+        if (isset($settingData['description'])) {
+            try {
+                $descriptionValidator = Validator::make(
+                    ['description' => $settingData['description']],
+                    ['description' => UpdateSettingRequest::getDescriptionValidationRules()]
+                );
+                $updateData = array_merge($updateData, $descriptionValidator->validate());
+            } catch (ValidationException $e) {
+                throw new SettingValueValidationException($e->getMessage());
+            }
+        }
+        $this->setting->update($updateData);
+    }
+
+}
