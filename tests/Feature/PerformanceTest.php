4e0f47dd679b810c86d8c823f093d116feba4e4d
xGrz <33755434+xGrz@users.noreply.github.com>
2024-02-14 20:30:09 +0100

    PerformanceTest.php added

---
+++ tests/Feature/PerformanceTest.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Cache;
+use Tests\TestCase;
+use xGrz\LaravelAppSettings\Models\Setting;
+use xGrz\LaravelAppSettings\Support\Facades\Config;
+use xGrz\LaravelAppSettings\Support\Facades\Settings;
+use xGrz\LaravelAppSettings\Support\Services\SyncService;
+
+class PerformanceTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        Config::publish();
+        SyncService::sync();
+    }
+
+    public function test_get_all_settings_from_db_performance_with_one_query()
+    {
+        $this->expectsDatabaseQueryCount(1);
+        Settings::invalidateCache();
+        Settings::getAll();
+        Settings::get('application.name');
+    }
+
+    public function test_get_all_settings_from_cache()
+    {
+        $this->expectsDatabaseQueryCount(0);
+        $this->assertGreaterThan('0', Settings::getAll());
+    }
+
+    public function test_get_one_settings_from_cache()
+    {
+        $setting = Setting::first(['key', 'value', 'type'])->makeHidden('type')->toArray();
+        $this->expectsDatabaseQueryCount(0);
+
+        $this->assertEquals(
+            $setting['value'],
+            Settings::get( $setting['key'])
+        );
+    }
+
+}
