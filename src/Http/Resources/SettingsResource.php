60038d5681168d58d6565788bfb2b202ade2a32a
xGrz <33755434+xGrz@users.noreply.github.com>
2024-02-21 12:15:28 +0100

    Added settings resource.

---
+++ src/Http/Resources/SettingsResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace xGrz\LaravelAppSettings\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SettingsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'key' => $this->key,
+            'groupName' => $this->groupName,
+            'type' => $this->type->value,
+            'typeName' => $this->type->name,
+            'description' => $this->description,
+            'value' => $this->value,
+            'viewableValue' => $this->viewableValue,
+        ];
+    }
+}
